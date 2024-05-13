4e81b2a90f9c444f4fbf034d50c93bf24b17a321
suvechha <suvechha.b@gmail.com>
2024-05-13 14:45:23 +1000

    Update vs_code.py

--- vs_code.py
+++ vs_code.py
@@ -7,9 +7,19 @@ df1 = pd.read_excel(r'C:\Users\sbhadra\AppData\Local\Programs\Python Scripts\Exc
 df2 = pd.read_excel(r'C:\Users\sbhadra\AppData\Local\Programs\Python Scripts\Excel\Q.xlsx')
 
 df3= df1.merge(df2,how = 'left', on = ['Key'])
-#print (df3)
 
-print(df3[['Employee Code_x','Timesheet Date','Date Start','Date End','Date Expiry','Comments','Skill Code','Qualification ID','Qualification ID Integer']])
 
-df3['Qualification ID'] = df3['Qualification ID'].map(str)
+df3['Employee Code_x']=df3['Employee Code_x']
+df3['Timesheet Date']=df3['Timesheet Date'].astype('datetime64[ns]')
+#df3['Date Start'] =  df3['Date Start'].dt.strftime('%m/%d/%Y')
+df3['Date Start'] = pd.to_datetime(df3['Date Start']).strftime('%Y-%m-%d')
+df3['Date End'] = pd.to_datetime(df3['Date End'], errors='coerce')
+df3['Date Expiry'] = pd.to_datetime(df3['Date Expiry'], errors='coerce')
+df3['Qualification ID']=df3['Qualification ID'].fillna(-1)
+df3['Qualification ID']=df3['Qualification ID'].astype(int)
+df3['Comments']=df3['Comments']
+df3['Skill Code']=df3['Skill Code']
+df3['Qualification ID Integer']=df3['Qualification ID Integer']
+print(df3.dtypes)
+print(df3)
 
